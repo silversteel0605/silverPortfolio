1921b6136243b7b580eef9bfc6fbd0f62e082717
Ethan J <silversteel0605@gmail.com>
2022-04-04 00:17:04 +0900

    edit_ver_0.02

--- js/myPage.js
+++ js/myPage.js
@@ -54,3 +54,23 @@ document.addEventListener('scroll', (e) => {
         link02.style.display = 'block';
     }
 });
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
+});
+
