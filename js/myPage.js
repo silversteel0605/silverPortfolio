7760970b45e36d10a3c395f92d122378296d44d6
Ethan J <silversteel0605@gmail.com>
2022-04-06 02:31:49 +0900

    edit ver_0.0.3

--- js/myPage.js
+++ js/myPage.js
@@ -55,22 +55,4 @@ document.addEventListener('scroll', (e) => {
     }
 });
 
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    })
-});
 
